b98c599a5f6c507b468d9f02ffd1d8b62111ad15
Breno Reis <breu1234@gmail.com>
2020-11-09 20:34:57 -0300

    Game Finished

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var board;
 const playerX = 'X';
 const playerO = 'O';
-const winCombos = [
+const winCombos = [ //combinações de posições vencedoras
     ["0", "1", "2"],
     ["3", "4", "5"],
     ["6", "7" ,"8"],
@@ -12,45 +12,60 @@ const winCombos = [
     ["2", "5" ,"8"]
 ]
 const cells = document.querySelectorAll('.cell');
-var currentPlayer = playerX;
+var currentPlayer = playerX; 
 var px = [];
 var po = [];
 
 startGame();
 
-function startGame() {
+function startGame() { 
     board = Array.from(Array(9).keys());
     for (var i = 0; i < cells.length; i++){
         cells[i].addEventListener('click', squareClick, false);
-        
     }
 }
 
 function squareClick(square) {
-    if(currentPlayer == playerX){
-        turn(square.target.id, playerX);
-        px.push(square.target.id);
-        if(checkWin(px)){
-            alert("O jogador X venceu!");
-        }
-        currentPlayer = playerO;
+    if((px.includes(square.target.id)) || (po.includes(square.target.id))){
+        //Não faz nada se o espaço já estiver ocupado
     } else {
-        turn(square.target.id, playerO);
-        po.push(square.target.id);
-        if(checkWin(po)){
-            alert("O jogador O venceu!");
+        if(currentPlayer == playerX){
+            mark(square.target.id, playerX);
+            px.push(square.target.id);
+            document.getElementById('turn').innerHTML = "Próximo a jogar: O";
+            if(checkWin(px)){
+                document.getElementById('turn').innerHTML = "Jogador X é o vencedor!";
+                blockBoard(playerX);
+            }
+            if(isFull()){
+                document.getElementById('turn').innerHTML = "O jogo empatou!";
+                setTimeout(function () { alert("Empate!") }, 1);
+            }
+            currentPlayer = playerO;
+        } else {
+            mark(square.target.id, playerO);
+            po.push(square.target.id);
+            document.getElementById('turn').innerHTML = "Próximo a jogar: X";
+            if(checkWin(po)){
+                document.getElementById('turn').innerHTML = "Jogador O é o vencedor!";
+                blockBoard(playerO);
+            }
+            if(isFull()){
+                document.getElementById('turn').innerHTML = "O jogo empatou!";
+                setTimeout(function () { alert("Empate!") }, 1);
+            }
+            currentPlayer = playerX;
         }
-        currentPlayer = playerX;
     }
-    //console.log(square.target.id);
 }
 
-function turn(squareId, player) {
+function mark(squareId, player) { //marca o espaço com o simbolo do jogador
 	board[squareId] = player;
     document.getElementById(squareId).innerText = player;
+    document.getElementById(squareId).style.cursor = "not-allowed";
 }
 
-function checkWin(player) {
+function checkWin(player) { //verifica se o player ganhou
     player.sort();
     let win = false;
 
@@ -63,11 +78,13 @@ function checkWin(player) {
     return win;
 }
 
-function isEqual (player, win){
+function isEqual (player, win){ //verifica se a combinação do player é igual a necessária para vencer
     let count = 0;
+    var combo;
 
     for(let i = 0; i < player.length; i++){
         if((player.includes(win[i]))){
+            combo = win;
             count++;
         } else {
             continue;
@@ -75,6 +92,26 @@ function isEqual (player, win){
     }
 
     if(count==3){
+        for(let i = 0; i < combo.length; i++){
+            document.getElementById(combo[i]).style.backgroundColor = "rgb(55, 224, 83)";
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function blockBoard(player){ //bloqueia o tabuleiro após o fim do jogo
+    px.push("0", "1", "2", "3", "4", "5", "6", "7", "8");
+    for(let i = 0; i < board.length; i++){
+        document.getElementById(i).style.cursor = "not-allowed";
+    }
+    setTimeout(function () { alert("O jogador " + player + " venceu!") }, 1);
+}
+
+function isFull(){
+    var sum = px.concat(po);
+    if(sum.length==9){
         return true;
     } else {
         return false;
